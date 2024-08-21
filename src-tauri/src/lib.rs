f659b77ba7aa79afae874712921b968398a95791
adraax <merlin.olivier71@gmail.com>
2024-08-21 16:24:18 +0200

    init project

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,28 @@
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+#[cfg(not(debug_assertions))]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+#[cfg(debug_assertions)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_devtools::init())
+        .plugin(tauri_plugin_devtools_app::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
