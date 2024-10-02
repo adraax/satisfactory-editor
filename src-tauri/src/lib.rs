261a4e88eb10071e1a5237043dd7958709b8237e
adraax <merlin.olivier71@gmail.com>
2024-10-02 23:49:37 +0200

    update dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,7 +23,6 @@ pub fn run() {
     }
 
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
