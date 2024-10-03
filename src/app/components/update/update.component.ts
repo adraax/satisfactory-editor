835b0b6ce19893b7d0756adec3cdac0671c20ba8
adraax <merlin.olivier71@gmail.com>
2024-10-03 03:06:28 +0200

    fix dev + fix progress bar not showing

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,6 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal, WritableSignal } from "@angular/core";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from "@angular/core";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { Router } from "@angular/router";
 import { check } from "@tauri-apps/plugin-updater";
 
@@ -15,30 +23,36 @@ type state = "loading" | "downloading";
   imports: [MatProgressSpinnerModule, MatProgressBarModule],
 })
 export class UpdateComponent implements OnInit {
+  private cf = inject(ChangeDetectorRef);
+
   public state: WritableSignal<state> = signal("loading");
-  public dowload = signal(0)
+  public dowload = signal(0);
 
   private router = inject(Router);
 
   async ngOnInit() {
-    let res = await check();
-    if (res) {
-      let downloaded = 0;
-      let contentLength = 0;
+    try {
+      let res = await check();
+      if (res) {
+        this.state.set("downloading");
+        let downloaded = 0;
+        let contentLength = 0;
 
-      await res.downloadAndInstall((event) => {
-        switch (event.event) {
-          case 'Started':
-            contentLength = event.data.contentLength!;
-            break;
-          
-          case 'Progress':
-            downloaded += event.data.chunkLength;
-            this.dowload.set(downloaded / contentLength * 100);
-            break;
-        }
-      });
-    } else {
+        await res.downloadAndInstall((event) => {
+          switch (event.event) {
+            case "Started":
+              contentLength = event.data.contentLength!;
+              break;
+
+            case "Progress":
+              downloaded += event.data.chunkLength;
+              this.dowload.set((downloaded / contentLength) * 100);
+              break;
+          }
+        });
+      }
+    } catch (e) {
+    } finally {
       this.router.navigateByUrl("main");
     }
   }
